01a46c1e153e2d33c4258224e75513938d5c35c0
Greg <gregory.avola@gmail.com>
2014-01-29 14:13:30 -0800

    Adding param example

--- basic_examples/index.php
+++ basic_examples/index.php
@@ -12,6 +12,8 @@ $ut = new UntappdPHP($client_id, $client_secret, $redirect_uri);
 
 // get the basic call without authetnication
 $info = $ut->get("/user/info/gregavola");
+// Pass params in the array as a second value
+// $info = $ut->get("/user/info/gregavola", array("limit" => 5));
 
 // fly away!
 print_r($info);
